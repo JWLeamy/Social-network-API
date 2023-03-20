55f3ea2d3b696a0c96c116573c1e846e8cad09fe
John Leamy <jwleamy@gmail.com>
2023-03-20 13:53:32 -0700

    user model

---
+++ Main/models/user.js
@@ -0,0 +1,52 @@
+const { Schema, model } = require('mongoose');
+const assignmentSchema = require('./Assignment');
+
+// Schema to create Student model
+const studentSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      validate:{
+        validator: validator.isEmail,
+        message: '{VALUE} is not a valid email',
+        isAsync: false
+      }
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
